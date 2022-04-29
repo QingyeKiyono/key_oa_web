2de513db66eba254f084cb225ca2633f80b1ac17
九月家的敖烈 <1604070358@qq.com>
2022-04-29 22:41:07 +0800

    引入element-plus和typescript-cookie

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// 引入Element-Plus
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+import router from "./router";
+
+createApp(App).use(router).use(ElementPlus).mount("#app");
