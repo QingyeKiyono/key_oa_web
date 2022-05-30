10f935434737d464ee9390abd0e4479313ce1c9a
九月家的敖烈 <1604070358@qq.com>
2022-05-30 20:52:17 +0800

    refactor(Router): 定义访问网页的行为
    1. 在访问网页之前检查token是否存在，如果不存在，那么跳转到登录页面

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import { getCookie } from "typescript-cookie";
+import { CookiePath } from "@/common";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,13 +18,13 @@ const router = createRouter({
   routes: routes,
 });
 
-router.beforeEach((to, from) => {
-  // if (to.path != "/login") {
-  //   // 如果不是首页的话，就检查token是否存在，如果不存在，就跳转到登录页面
-  //   if (getCookie("token") == null) {
-  //     router.push("/login").then();
-  //   }
-  // }
+router.beforeEach((to) => {
+  if (to.path != "/login") {
+    // 如果不是首页的话，就检查token是否存在，如果不存在，就跳转到登录页面
+    if (getCookie(CookiePath.token) == null) {
+      router.push("/login").then();
+    }
+  }
 
   if (to.meta.title) {
     document.title = to.meta.title as string;
