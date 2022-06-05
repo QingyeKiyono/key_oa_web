b0b70b81b968bab182e3ba38967f17c6bc528cdb
九月家的敖烈 <1604070358@qq.com>
2022-06-05 16:26:06 +0800

    feature(UI): 更改基本布局
    1. 添加签到按钮和通知按钮（功能未实现）
    2. 添加侧边栏导航（功能未实现）

--- src/views/BasePage.vue
+++ src/views/BasePage.vue
@@ -5,12 +5,39 @@
     ></v-app-bar-nav-icon>
     <v-toolbar-title>KeyOA</v-toolbar-title>
     <v-spacer></v-spacer>
+    <v-btn>
+      <v-icon>mdi-check</v-icon>
+    </v-btn>
+    <v-btn>
+      <v-icon>mdi-bell</v-icon>
+    </v-btn>
     <v-btn @click="showLogoutConfirm = true">
-      <span>Logout</span>
+      <span>退出登录</span>
       <v-icon>mdi-exit-to-app</v-icon>
     </v-btn>
   </v-toolbar>
-  <v-navigation-drawer v-model="showSideDrawer" app>123123</v-navigation-drawer>
+  <v-navigation-drawer v-model="showSideDrawer" app :expand-on-hover="true">
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-account"
+        :title="briefProfile.name"
+        :subtitle="briefProfile.jobNumber"
+        @click="gotoProfile"
+      ></v-list-item>
+      <v-divider></v-divider>
+      <v-list :nav="true">
+        <v-list-item
+          v-for="navRoute in navigationRoutes"
+          :key="navRoute.route"
+          :prepend-icon="navRoute.icon"
+          :title="navRoute.name"
+          :value="navRoute.route"
+          :link="true"
+          :to="navRoute.route"
+        ></v-list-item>
+      </v-list>
+    </v-list>
+  </v-navigation-drawer>
   <slot></slot>
   <v-dialog v-model="showLogoutConfirm">
     <v-card>
@@ -26,16 +53,19 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import router from "@/routes";
 import { jsonResRequest } from "@/utils/WebUtil";
 import { removeCookie } from "typescript-cookie";
 import { CookieName } from "@/common";
 
+// 是否显示侧边导航栏
 const showSideDrawer = ref(true);
 
+// 确认退出登录的对话框是否显示
 const showLogoutConfirm = ref(false);
 
+// 退出登录按钮被点击
 let logout = () => {
   // 发送退出登录的请求
   jsonResRequest<void>({ url: "/logout", method: "POST" }).then((r) => {
@@ -48,6 +78,48 @@ let logout = () => {
   // 重新导航到登录页面
   router.push("/login");
 };
+
+// 简要Profile信息（姓名、工号）
+const briefProfile = ref({
+  name: "芦畅",
+  jobNumber: "20221390",
+});
+
+// 简要Profile信息（侧边栏第一项）被点击
+let gotoProfile = () => {
+  console.log("going to profile...");
+};
+
+// 侧边栏的导航路由
+interface NavigationRouteRecord {
+  name: String;
+  route: String;
+  icon?: String;
+}
+
+// 侧边栏的路由信息
+const navigationRoutes = reactive<Array<NavigationRouteRecord>>([
+  {
+    name: "My files",
+    route: "/files",
+    icon: "mdi-folder",
+  },
+  {
+    name: "Shared with me",
+    route: "/share",
+    icon: "mdi-account-multiple",
+  },
+  {
+    name: "Starred",
+    route: "/star",
+    icon: "mdi-star",
+  },
+]);
+
+onMounted(() => {
+  // 这里需要获取一些后台的资源
+  console.log("mounted...");
+});
 </script>
 
 <style scoped></style>
