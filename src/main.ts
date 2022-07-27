13c7f02c3006f0e149ec74754bea4c7ccaf0274e
九月家的敖烈 <1604070358@qq.com>
2022-07-27 15:58:45 +0800

    perf(UI): 提高加载速度
    使用压缩后的css文件以提高加载速度，减少响应时间

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import App from "./App.vue";
 
 // Vuetify
 import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
+import "@mdi/font/css/materialdesignicons.min.css";
 import { createVuetify } from "vuetify";
 // Vuetify Components
 import * as components from "vuetify/components";
