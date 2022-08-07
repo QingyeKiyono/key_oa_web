fc5126c8e2df11f52cc55e42f8132c851e3b7533
九月家的敖烈 <1604070358@qq.com>
2022-08-07 16:10:18 +0800

    fix(UI): 修整按钮UI
    修整按钮的UI显示

--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -1,50 +1,61 @@
 <template>
-  <v-container class="ma-0 pa-0" fluid>
-    <v-row>
-      <v-col>
-        <v-btn @click="router.push('/profile/new')"> 新建员工信息 </v-btn>
+  <v-container class="ma-1 pa-0" fluid>
+    <!--suppress HtmlUnknownBooleanAttribute -->
+    <v-row justify="start" no-gutters>
+      <v-col cols="1">
+        <v-btn
+          @click="router.push('/profile/new')"
+          class="d-inline"
+          variant="outlined"
+        >
+          新建员工信息
+        </v-btn>
       </v-col>
-      <v-col>
-        <v-btn @click="display" :disabled="state.selectEmployees.length === 0">
+      <v-col cols="1">
+        <v-btn
+          @click="display"
+          variant="outlined"
+          :disabled="state.selectEmployees.length === 0"
+        >
           删除
         </v-btn>
       </v-col>
     </v-row>
     <v-table :fixed-header="true" class="pa-1">
       <thead>
-      <tr>
-        <th class="text-left"></th>
-        <th class="text-left v-col-1">姓名</th>
-        <th class="text-left">工号</th>
-        <th class="text-left">电话号码</th>
-        <th class="text-left">出生日期</th>
-        <th class="text-left v-col-2">电子邮箱</th>
-        <th class="text-left v-col-4">操作</th>
-      </tr>
+        <tr>
+          <th class="text-left"></th>
+          <th class="text-left v-col-1">姓名</th>
+          <th class="text-left">工号</th>
+          <th class="text-left">电话号码</th>
+          <th class="text-left">出生日期</th>
+          <th class="text-left v-col-2">电子邮箱</th>
+          <th class="text-left v-col-4">操作</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="employee in state.employeeList" :key="employee.id">
-        <td>
-          <v-checkbox-btn
-            :disabled="employee.jobNumber === store.jobNumber"
-            v-model="state.selectEmployees"
-            :value="employee.jobNumber"
-          ></v-checkbox-btn>
-        </td>
-        <td>{{ employee.name }}</td>
-        <td>{{ employee.jobNumber }}</td>
-        <td>{{ employee.phone }}</td>
-        <td>{{ employee.birthday }}</td>
-        <td>{{ employee.email }}</td>
-        <td>
-          <v-row>
-            <v-btn @click="router.push(`/profile/${employee.jobNumber}`)">
-              查看详细信息
-            </v-btn>
-            <v-btn class="offset-1">发送消息</v-btn>
-          </v-row>
-        </td>
-      </tr>
+        <tr v-for="employee in state.employeeList" :key="employee.id">
+          <td>
+            <v-checkbox-btn
+              :disabled="employee.jobNumber === store.jobNumber"
+              v-model="state.selectEmployees"
+              :value="employee.jobNumber"
+            ></v-checkbox-btn>
+          </td>
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.jobNumber }}</td>
+          <td>{{ employee.phone }}</td>
+          <td>{{ employee.birthday }}</td>
+          <td>{{ employee.email }}</td>
+          <td>
+            <v-row>
+              <v-btn @click="router.push(`/profile/${employee.jobNumber}`)">
+                查看详细信息
+              </v-btn>
+              <v-btn class="offset-1">发送消息</v-btn>
+            </v-row>
+          </td>
+        </tr>
       </tbody>
     </v-table>
     <v-pagination
