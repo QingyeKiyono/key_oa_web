837f9ea9f2fc1344f4f42db131d2412b81300038
jyjdal <1604070358@qq.com>
2023-02-13 15:37:34 +0800

    Refine data table's UI.

--- src/views/List/PageRes.vue
+++ src/views/List/PageRes.vue
@@ -1,9 +1,20 @@
+<!--suppress VueUnrecognizedSlot -->
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="pages"
-    @update:options="test"
-  ></v-data-table>
+  <v-data-table :headers="headers" :items="pages" @update:options="test" class="ma-3">
+    <template v-slot:item.pageGroup="{ item }">
+      <v-icon>{{ item.raw.pageGroup ? "mdi-check" : "mdi-close" }}</v-icon>
+    </template>
+    <template v-slot:item.icon="{ item }">
+      <v-icon>{{ item.raw.icon }}</v-icon>
+    </template>
+    <template v-slot:item.parent="{ parent }">
+      {{ parent == null ? "无" : parent.id }}
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon size="small" class="me-2">mdi-pencil</v-icon>
+      <v-icon size="small">mdi-delete</v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script setup lang="ts">
@@ -13,30 +24,13 @@ import {jsonResRequest} from "@/utils";
 
 // 表格标题
 const headers: Array<DataTableHeader> = [
-  {
-    title: "Url",
-    key: "url",
-    align: "start",
-    sortable: false,
-  },
-  {
-    title: "图标",
-    key: "icon",
-    align: "start",
-    sortable: false,
-  },
-  {
-    title: "图标组",
-    key: "pageGroup",
-    align: "start",
-    sortable: true,
-  },
-  {
-    title: "父元素",
-    key: "parent",
-    align: "start",
-    sortable: false,
-  },
+  { title: "Id", key: "id" },
+  { title: "Url", key: "url" },
+  { title: "图标", key: "icon" },
+  { title: "描述", key: "description" },
+  { title: "图标组", key: "pageGroup", sortable: true },
+  { title: "父元素", key: "parent" },
+  { title: "操作", key: "actions" },
 ];
 
 const pages = ref(new Array<PageRes>());
@@ -51,7 +45,6 @@ function fetchPages(page: Number, size: Number) {
 }
 
 function test(paras: Object) {
-  console.log(paras);
   fetchPages((paras as any).page, (paras as any).itemsPerPage);
 }
 
