b362c6d352331c22e961244ae15bed276fe0e6c2
jyjdal <1604070358@qq.com>
2023-02-13 12:42:42 +0800

    Add type definition for VDataTable's header property.

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -5,6 +5,15 @@ interface JsonResponse<T = any> {
 }
 export { type JsonResponse };
 
+interface VDataTableHeader {
+  title: String;
+  sortable: Boolean;
+  align: "start" | "end";
+  key: String;
+}
+
+export { type VDataTableHeader };
+
 // Entity types.
 export { type Employee, type PageRes } from "./Entity";
 
