a43b55441b435dbfc1ea05f80bb4c112e43100ad
jyjdal <1604070358@qq.com>
2023-02-13 12:42:18 +0800

    Add comment.

--- src/common/Constant.ts
+++ src/common/Constant.ts
@@ -1,9 +1,12 @@
 const cookieBase = "key_oa";
 
+// 用于记录Jwt
 const token = `${cookieBase}.login`;
 
+// 记录当前登录员工的工号
 const jobNumber = `${cookieBase}.jobNumber`;
 
+// 用于记录当前颜色主题
 const theme = `${cookieBase}.theme`;
 
 const CookieName = {
