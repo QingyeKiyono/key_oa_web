9740006cdc83433e7db706eb1b1d71070fbbb096
jyjdal <1604070358@qq.com>
2023-02-14 14:25:35 +0800

    Reimplement page: Employees.vue.

--- src/views/List/Employees.vue
+++ src/views/List/Employees.vue
@@ -1,124 +1,101 @@
+<!--suppress VueUnrecognizedSlot -->
 <template>
-  <v-container class="ma-1 pa-0" fluid>
-    <v-row justify="start" no-gutters>
-      <v-col cols="1">
-        <v-btn
-          @click="router.push('/profile/new')"
-          class="d-inline"
-          variant="outlined"
-        >
-          新建员工信息
-        </v-btn>
-      </v-col>
-      <v-col cols="1">
-        <v-btn
-          @click="deleteEmployees"
-          variant="outlined"
-          :disabled="state.selectEmployees.length === 0"
-        >
-          删除
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-table :fixed-header="true" class="pa-1">
-      <thead>
-        <tr>
-          <th class="text-left"></th>
-          <th class="text-left v-col-1">姓名</th>
-          <th class="text-left">工号</th>
-          <th class="text-left">电话号码</th>
-          <th class="text-left">出生日期</th>
-          <th class="text-left v-col-2">电子邮箱</th>
-          <th class="text-left v-col-4">操作</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="employee in state.employeeList" :key="employee.id">
-          <td>
-            <v-checkbox-btn
-              :disabled="employee.jobNumber === store.jobNumber"
-              v-model="state.selectEmployees"
-              :value="employee.jobNumber"
-            ></v-checkbox-btn>
-          </td>
-          <td>{{ employee.name }}</td>
-          <td>{{ employee.jobNumber }}</td>
-          <td>{{ employee.phone }}</td>
-          <td>{{ employee.birthday }}</td>
-          <td>{{ employee.email }}</td>
-          <td>
-            <v-row>
-              <v-btn @click="router.push(`/profile/${employee.jobNumber}`)">
-                查看详细信息
-              </v-btn>
-              <v-btn class="offset-1">发送消息</v-btn>
-            </v-row>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
-    <v-pagination
-      v-model="state.page"
-      :length="state.length"
-      v-on:update:modelValue="updatePage"
-    ></v-pagination>
-  </v-container>
+  <v-data-table-server
+    :items-length="count"
+    v-model:items-per-page="size"
+    :headers="headers"
+    :items="employees"
+    :loading="loading"
+    @update:options="updateTable"
+    class="ma-3"
+  >
+    <template v-slot:top>
+      <v-toolbar density="compact">
+        <v-toolbar-title>员工列表</v-toolbar-title>
+        <v-divider inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-btn>新建员工信息</v-btn>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.verified="{ item }">
+      <v-icon>{{ item.raw.verified ? "mdi-check" : "mdi-close" }}</v-icon>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-btn
+        size="x-small"
+        variant="outlined"
+        icon="mdi-pencil"
+        class="mr-2"
+      ></v-btn>
+      <v-btn
+        size="x-small"
+        variant="outlined"
+        icon="mdi-delete"
+        :disabled="item.raw.jobNumber === login.loginEmployee.jobNumber"
+        @click="deleteEmployee"
+      >
+      </v-btn>
+    </template>
+  </v-data-table-server>
 </template>
 
 <script setup lang="ts">
-import {onMounted, reactive} from "vue";
+import {onMounted, ref} from "vue";
 
 import {jsonResRequest} from "@/utils";
-import {Employee} from "@/common";
-import router from "@/router";
+import {DataTableHeader, Employee} from "@/common";
 import {useLoginStore} from "@/store";
 
-const store = useLoginStore();
+const headers: Array<DataTableHeader> = [
+  { title: "Id", key: "id" },
+  { title: "姓名", key: "name" },
+  { title: "工号", key: "jobNumber" },
+  { title: "电话号码", key: "phone" },
+  { title: "电子邮箱", key: "email" },
+  { title: "出生日期", key: "birthday" },
+  { title: "激活", key: "verified" },
+  { title: "操作", key: "actions" },
+];
 
-let state = reactive({
-  // 员工的列表
-  employeeList: new Array<Employee>(),
-  // 和分页有关的变量，分别是：页号；页数；每页员工数
-  page: 1,
-  length: 1,
-  pageSize: 8,
-  selectEmployees: [] as Array<string>,
-});
+const employees = ref(new Array<Employee>()); // 获取到的员工信息
+const count = ref(0); // 员工总数
+const size = ref(10); // 每页展示员工数
+
+const loading = ref(false); // 表格的加载状态
+
+const login = useLoginStore(); // 当前登录状态
 
 // 页面更新后的动作，即获取对应页面的员工列表
-function updatePage(page: Number): void {
+function fetchEmployees(page: Number, size: Number) {
+  loading.value = true;
+  // Load data
   jsonResRequest<Array<Employee>>({
     url: "/employees",
-    params: {
-      page: page,
-      size: state.pageSize,
-    },
+    params: { page, size },
   }).then((res) => {
-    state.employeeList = res.data;
+    employees.value = res.data;
   });
+
+  // Data loaded
+  loading.value = false;
 }
 
-function deleteEmployees() {
-  console.log(state.selectEmployees.length);
-  jsonResRequest({
-    url: "/employees/:deleteBatch",
-    method: "DELETE",
-    data: JSON.stringify(state.selectEmployees),
-  }).then(() => {
-    router.go(0);
-  });
+function updateTable(paras: Object) {
+  fetchEmployees((paras as any).page, (paras as any).itemsPerPage);
+}
+
+function deleteEmployee() {
+  console.log(123);
 }
 
 onMounted(() => {
-  // 首先获取员工数量，确定分页有几页
+  // 获取员工总数
   jsonResRequest<number>({
     url: "/employees/count",
-  }).then((res) => {
-    state.length = Math.ceil(res.data / state.pageSize);
-  });
+  }).then((res) => (count.value = res.data));
 
   // 然后获取第一页的员工
-  updatePage(state.page);
+  fetchEmployees(1, size.value);
 });
 </script>
 
