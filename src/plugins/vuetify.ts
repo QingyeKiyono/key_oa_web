4d235ed37a66301f963739d9a684ab045ec2cbb5
jyjdal <1604070358@qq.com>
2023-02-14 14:09:06 +0800

    Import component VDataTableServer.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,12 +4,13 @@ import "vuetify/styles";
 
 import {createVuetify} from "vuetify";
 import {md3} from "vuetify/blueprints";
-import {VDataTable} from "vuetify/labs/VDataTable";
+import {VDataTable, VDataTableServer} from "vuetify/labs/VDataTable";
 
 export default createVuetify({
   blueprint: md3,
   components: {
     VDataTable,
+    VDataTableServer,
   },
   theme: {
     defaultTheme: "light",
