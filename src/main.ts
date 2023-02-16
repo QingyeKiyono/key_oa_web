ebcd59b0314a96d382ff80984b76297b881575de
jyjdal <1604070358@qq.com>
2023-02-16 18:53:20 +0800

    Reformat code.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
-import App from "./App.vue";
-
-// Composables
 import { createApp } from "vue";
 
-// Plugins
+import App from "./App.vue";
+
 import { registerPlugins } from "@/plugins";
 
 const app = createApp(App);
-
 registerPlugins(app);
-
 app.mount("#app");
