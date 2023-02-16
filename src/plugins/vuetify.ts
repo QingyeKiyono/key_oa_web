719daa6b729da5b023188cfa9d3a162a584dfca7
jyjdal <1604070358@qq.com>
2023-02-16 18:52:29 +0800

    Reformat code.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,9 +2,9 @@
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
-import {createVuetify} from "vuetify";
-import {md3} from "vuetify/blueprints";
-import {VDataTable, VDataTableServer} from "vuetify/labs/VDataTable";
+import { createVuetify } from "vuetify";
+import { md3 } from "vuetify/blueprints";
+import { VDataTable, VDataTableServer } from "vuetify/labs/VDataTable";
 
 export default createVuetify({
   blueprint: md3,
