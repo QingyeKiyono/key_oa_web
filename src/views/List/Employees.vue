fb0c3f8ee8ec37074204fb0d8af7f1659f10d67c
jyjdal <1604070358@qq.com>
2023-02-18 21:16:29 +0800

    Add function to delete employee.

--- src/views/List/Employees.vue
+++ src/views/List/Employees.vue
@@ -27,7 +27,7 @@
       </v-chip>
     </template>
     <template v-slot:item.verified="{ item }">
-      <v-icon>{{ item.raw.verified ? "mdi-check" : "mdi-close" }}</v-icon>
+      <v-icon>{{ item.raw.activated ? "mdi-check" : "mdi-close" }}</v-icon>
     </template>
     <template v-slot:item.actions="{ item }">
       <v-btn
@@ -41,19 +41,30 @@
         variant="outlined"
         icon="mdi-delete"
         :disabled="item.raw.jobNumber === login.loginEmployee.jobNumber"
-        @click="deleteEmployee"
+        @click="deleteEmployee(item.raw)"
       >
       </v-btn>
     </template>
   </v-data-table-server>
+  <v-dialog v-model="dialogDelete" width="auto">
+    <v-card>
+      <v-card-text>确定删除当前员工吗？</v-card-text>
+      <v-card-actions>
+        <v-btn @click="deleteConfirm">确定</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn @click="dialogDelete = false">取消</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, Ref, ref } from "vue";
 
 import { jsonResRequest } from "@/utils";
 import { DataTableHeader, Employee } from "@/common";
 import { useLoginStore } from "@/plugins/store";
+import { DataTableOptions } from "@/common/types";
 
 const headers: Array<DataTableHeader> = [
   { title: "Id", key: "id" },
@@ -70,10 +81,13 @@ const headers: Array<DataTableHeader> = [
 const employees = ref(new Array<Employee>()); // 获取到的员工信息
 const count = ref(0); // 员工总数
 const size = ref(10); // 每页展示员工数
-
 const loading = ref(false); // 表格的加载状态
+const options: Ref<DataTableOptions> = ref({ page: 1, itemsPerPage: 10 });
 
 const login = useLoginStore(); // 当前登录状态
+
+const dialogDelete = ref(false);
+const selectedEmployee: Ref<Employee | null> = ref(null); // 选中删除的员工
 
 // 页面更新后的动作，即获取对应页面的员工列表
 function fetchEmployees(page: Number, size: Number) {
@@ -90,12 +104,25 @@ function fetchEmployees(page: Number, size: Number) {
   loading.value = false;
 }
 
-function updateTable(paras: Object) {
-  fetchEmployees((paras as any).page, (paras as any).itemsPerPage);
+function updateTable(paras: DataTableOptions) {
+  options.value = paras;
+  fetchEmployees(paras.page, paras.itemsPerPage);
 }
 
-function deleteEmployee() {
-  console.log(123);
+function deleteEmployee(employee: Employee) {
+  selectedEmployee.value = employee;
+  dialogDelete.value = true;
+}
+
+function deleteConfirm() {
+  jsonResRequest<void>({
+    url: "/employees",
+    method: "DELETE",
+    data: JSON.stringify([selectedEmployee.value!.jobNumber]),
+  }).then(() => {
+    dialogDelete.value = false;
+    updateTable(options.value);
+  });
 }
 
 onMounted(() => {
